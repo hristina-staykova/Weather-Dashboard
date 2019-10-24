ea2c88bfb3539c8b2083bf4552f86d5fac3af9cb
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-24 12:01:22 +0200

    display of weather info - T, H, WS

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
   var cityName = $("#cityName");
   var searchButton = $("#searchbtn");
+  var toDay = moment().format("DD/M/YYYY");
 
   searchButton.on("click", function(event) {
-    cityName = $("#cityName").val();
+    cityName = cityName.val();
+    $("#cityNameEl").text(cityName + " " + toDay);
     var queryURL =
       "http://api.openweathermap.org/data/2.5/weather?q=" +
       cityName +
-      "&APPID=66b38c70f7e47f3330c092ef356a429f";
-    console.log(cityName);
+      "&units=metric&APPID=66b38c70f7e47f3330c092ef356a429f";
     $.ajax({ url: queryURL, method: "GET" }).then(function getData(response) {
       console.log(response);
+      var cityTemperature = response.main.temp;
+      var cityHumidity = response.main.humidity;
+      var cityWindSpeed = response.wind.speed;
+      var cityLat = response.coord.lat;
+      var cityLon = response.coord.lon;
+
+      $("#temperature").text("Temperature: " + cityTemperature) + " C";
+      $("#humidity").text("Humidity: " + cityHumidity + "%");
+      $("#speed").text("Wind Speed: " + cityWindSpeed + "km/h");
+      $("#uv").text("UN Index: ");
     });
   });
 });
